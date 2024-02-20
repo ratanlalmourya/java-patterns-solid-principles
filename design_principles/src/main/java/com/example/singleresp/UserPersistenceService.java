e652b3a406be6c51e7f93498c80ccabb8c2230ad
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-02-20 14:46:01 +0000

    Single Responsebility Principal

--- design_principles/src/main/java/com/example/singleresp/UserPersistenceService.java
+++ design_principles/src/main/java/com/example/singleresp/UserPersistenceService.java
@@ -1,6 +1,8 @@
 package com.example.singleresp;
 
 public class UserPersistenceService {
-
-	
+    private Store store = new Store();
+    void  saveUser(User user) {
+        store.store(user);
+    }
 }
