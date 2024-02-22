165dd6169549055c14551bcf41b9d96ec627493f
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-02-22 15:38:00 +0000

    Dependenjcy inversion principle applied

--- design_principles/src/main/java/com/example/depinv/MessagePrinter.java
+++ design_principles/src/main/java/com/example/depinv/MessagePrinter.java
@@ -1,17 +1,13 @@
 package com.example.depinv;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MessagePrinter {
 
 	//Writes message to a file
-	public void writeMessage(Message msg, String fileName) throws IOException {
-		Formatter formatter = new JSONFormatter();//creates formatter
-		try(PrintWriter writer = new PrintWriter(new FileWriter(fileName))) { //creates print writer
+	public void writeMessage(Message msg, Formatter formatter,PrintWriter writer) throws IOException {
 			writer.println(formatter.format(msg)); //formats and writes message
 			writer.flush();
-		}
 	}
 }
