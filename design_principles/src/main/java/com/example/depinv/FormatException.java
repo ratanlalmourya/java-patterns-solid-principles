3692ae24158d7976a4dc04665630e15228c4ebae
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-02-22 15:27:04 +0000

    code for dependency injection setup

---
+++ design_principles/src/main/java/com/example/depinv/FormatException.java
@@ -0,0 +1,11 @@
+package com.example.depinv;
+
+import java.io.IOException;
+
+//Thrown by formatter
+public class FormatException extends IOException {
+	
+	public FormatException(Exception cause) {
+		super(cause);
+	}
+}
