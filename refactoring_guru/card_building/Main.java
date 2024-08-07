091043dc4a38f2afb10e3474cbbc2d0cb7c1188c
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-08-07 03:35:27 +0000

    Car building

---
+++ refactoring_guru/card_building/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Car Building");
+
+        Engine engine = new Engine(10, 23);
+        System.out.println(engine.getMileage());
+    }
+}
